eace4bb48bb3be97edb08710e28276d6a9b84e6b
Vahdetyldz <vahdet11766@gmail.com>
2025-03-05 15:55:49 +0300

    last update

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -62,7 +62,8 @@
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
-                        <p>{{$blog->content}}</p>
+                        <p><?php echo (nl2br($blog->content)) ?></p>
+                        
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -29,20 +29,13 @@ Route::get('/createBlog', function () {
 Route::get('/blog-myBlogs/{id}',[BlogController::class, 'myindex'])->name('myblogs');
 
 
-// Kullanıcı İşlemleri
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-//Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Blog İşlemleri
-Route::middleware('web')->group(function () {
-    Route::get('/blogs/load-more-blogs', [BlogController::class, 'loadMoreBlogs']);
-    Route::get('/blog-content/{id}', [BlogController::class, 'GetById']);
-    
-    //Route::get('/blogs', [BlogController::class, 'index'])->name('home');
-    Route::post('/blogs-store', [BlogController::class, 'store'])->name('post.store');
-    Route::get('/blogs/{id}/edit', [BlogController::class, 'edit'])->name('post.edit');
-    Route::post('/blogs/{id}/update', [BlogController::class, 'update'])->name('post.update');
-    Route::delete('/blogs/{id}', [BlogController::class, 'destroy'])->name('post.destroy');
-});
+Route::get('/blogs/load-more-blogs', [BlogController::class, 'loadMoreBlogs']);
+Route::get('/blog-content/{id}', [BlogController::class, 'GetById']);
+Route::post('/blogs-store', [BlogController::class, 'store'])->name('post.store');
+Route::get('/blogs/{id}/edit', [BlogController::class, 'edit'])->name('post.edit');
+Route::post('/blogs/{id}/update', [BlogController::class, 'update'])->name('post.update');
+Route::delete('/blogs/{id}', [BlogController::class, 'destroy'])->name('post.destroy');
