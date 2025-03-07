adfbb29cc2fccaa71620608fa8ac33f79a1c5e43
Vahdetyldz <vahdet11766@gmail.com>
2025-03-07 17:11:55 +0300

    last update

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
-use Illuminate\Database\Eloquent\Casts\Json;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
